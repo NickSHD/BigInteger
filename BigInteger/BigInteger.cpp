3836cfdccfdfed76605f61026ba3fab450c77ff9
Nickolay <36867173+NickSHD@users.noreply.github.com>
2018-09-21 21:47:15 +0300

    Update BigInteger.cpp

--- BigInteger/BigInteger.cpp
+++ BigInteger/BigInteger.cpp
@@ -21,11 +21,9 @@ BigInteger::BigInteger(int a) {
 	sign = false;
 	if (a == 0) {
 		data.push_back(0);
-	}
-	else if (a > 0) {
+	} else if (a > 0) {
 		data.push_back((unsigned)a);
-	}
-	else {
+	} else {
 		data.push_back((unsigned)(-a));
 		sign = true;
 	}
@@ -67,12 +65,10 @@ std::string BigInteger::to_string() const {
 	}
 	if (s.empty()) {
 		return "0";
-	}
-	else {
+	} else {
 		return s;
 	}
 }
-
 
 BigInteger::BigInteger(std::string const &str) {
 	std::string s(str);
@@ -82,8 +78,7 @@ BigInteger::BigInteger(std::string const &str) {
 	if (s[0] == '-') {
 		sign = true;
 		s.erase(s.begin());
-	}
-	else {
+	} else {
 		sign = false;
 	}
 	while (s.length() % 9 != 0) {
@@ -114,13 +109,11 @@ bool BigInteger::operator<(const BigInteger &right) const {
 	if (this->sign) {
 		if (right.sign) return ((-right) < (-*(this)));
 		else return true;
-	}
-	else if (right.sign) return false;
+	} else if (right.sign) return false;
 	else {
 		if ((int)this->data.size() != (int)right.data.size()) {
 			return (int)this->data.size() < (int)right.data.size();
-		}
-		else {
+		} else {
 			for (int i = (int)this->data.size() - 1; i >= 0; --i) {
 				if (this->data[i] != right.data[i]) return this->data[i] < right.data[i];
 			}
@@ -157,7 +150,6 @@ BigInteger BigInteger::operator-() const {
 	return BigInteger(data, !sign);
 }
 
-
 void BigInteger::make_right() {
 	if (data.empty()) {
 		data.push_back(0);
@@ -168,7 +160,6 @@ void BigInteger::make_right() {
 	data.resize(i + 1);
 }
 
-/*
 BigInteger& BigInteger::operator*=(ui const &b) {
 	BigInteger res;
 	res.sign = this->sign;
@@ -180,8 +171,6 @@ BigInteger& BigInteger::operator*=(ui const &b) {
 	res.make_right();
 	return *this;
 }
-
-
 
 BigInteger BigInteger::operator~() const {
 	const BigInteger tmp_big(1);
@@ -219,7 +208,7 @@ BigInteger &BigInteger::operator-=(BigInteger const &rhs) {
 		return *this;
 	}
 	if (sign != rhs.sign) {
-		return *this += -rhs;
+return *this += -rhs;
 	}
 	size_t len = std::max(data.size(), rhs.data.size());
 	size_t oldSize = data.size();
@@ -231,8 +220,7 @@ BigInteger &BigInteger::operator-=(BigInteger const &rhs) {
 	for (size_t i = 0; i < len; ++i) {
 		if (data[i] > rhs.data[i]) {
 			data[i] -= rhs.data[i];
-		}
-		else {
+		} else {
 			size_t p = i + 1;
 			while (data[p] == 0) {
 				data[p++] += kBase - 1;
@@ -259,305 +247,339 @@ BigInteger &BigInteger::operator*=(BigInteger const &rhs) {
 }
 
 ui getNumber(std::vector<ui> const &a, size_t const &ind) {
-return ind < a.size() ? a[ind] : (ui)0;
+	return ind < a.size() ? a[ind] : (ui)0;
 }
 
-ui getTrial(ui const &a, ui const &b, ui const &c) {
-ull res = (((ull)a << BigInteger::kLogBase) + b) / c;
-if (res > BigInteger::kBase - 1) {
-res = BigInteger::kBase - 1;
-}
-return (ui)res;
+std::pair<BigInteger, ui> sDiv(BigInteger const & a, ui const & b)
+//std::pair<BigInteger, ui> sDiv(BigInteger const &a, unsigned int const &b) 
+{
+	BigInteger res;
+	res.sign = a.sign;
+	unsigned long long cur = 0;
+	size_t temp = 0;
+	for (size_t i = a.data.size() - 1; --i; i >= 0) {
+		temp = cur * BigInteger::kBase + a.data[i];
+		res.data[i] = temp / b;
+		cur *= BigInteger::kBase;
+		cur += i;
+		res.data.push_back((unsigned)(cur / b));
+		cur -= res.data.back() * b;
+	}
+	res.make_right();
+	return { res, cur };
 }
 
-std::pair<BigInteger, ui> sDiv(BigInteger const &a, unsigned  int const &b) {
-BigInteger res;
-res.sign = a.sign;
-unsigned long long cur = 0;
-size_t temp = 0;
-for (size_t i = a.data.size() - 1; --i; i >= 0) {
-temp = cur * BigInteger::kBase + a.data[i];
-res.data[i] = temp / b;
-
-cur *= BigInteger::kBase;
-cur += i;
-res.data.push_back((unsigned)(cur / b));
-cur -= res.data.back() * b;
-}
-res.make_right();
-return { res, cur };
+void myDiv(BigInteger const &a, BigInteger &b, BigInteger &res, BigInteger &cur) {
+	if (a.data.size() < b.data.size()) {
+		res = a;
+		return;
+	}
+	if (b.data.size() == 1) {
+		std::pair<BigInteger, ui> p = sDiv(a, b.data[0]);
+		res = p.first;
+		cur.data[0] = p.second;
+		return;
+	}
+	BigInteger u = a;
+	ull n = a.data.size(), m = u.data.size() - b.data.size();
+	ui scale = BigInteger::kBase / (b.data[n - 1] + 1); //нормализация
+	if (scale > 1) {   //нормализация
+		u *= scale;
+		b *= scale;
+	}
+	ull uJ, vJ, i;            //vJ - тек сдвиг B относ U(при вычитании), инд очередной цифры частного
+	ull temp1, temp2, temp;   //uJ - тек цифра U
+	ui qGuess, r;             //догадка для частного и остаток
+	ui borrow, carry; //переносы
+	for (vJ = m, uJ = n + vJ; vJ >= 0; --vJ, --uJ) {
+		qGuess = (u.data[uJ] * BigInteger::kBase + u.data[uJ - 1]) / b.data[n - 1];
+		r = (u.data[uJ] * BigInteger::kBase + u.data[uJ - 1]) % b.data[n - 1];
+		while (r < BigInteger::kBase) {
+			temp2 = b.data[n - 2] * qGuess;
+			temp1 = r * BigInteger::kBase + u.data[uJ - 2];
+			if ((temp2 > temp1) || (qGuess == BigInteger::kBase)) {
+				--qGuess;                 //не работает, qGuess уменьшим
+				r += b.data[n - 1];       //новый остаток
+			} else break;     //qGuess правильное или на единицу больше
+		}
+		//U - B * qGuess начиная с vJ+i позиции
+		carry = 0, borrow = 0;
+		ui *uShift = &u.data[vJ];
+		for (i = 0; i < n; ++i) {
+			temp1 = b.data[i] * qGuess + carry;
+			carry = temp1 / BigInteger::kBase;
+			temp1 -= carry * BigInteger::kBase;
+			temp2 = uShift[i] - temp1 + borrow;
+			if (temp2 < 0) {
+				uShift[i] = temp2 + BigInteger::kBase;
+				borrow = -1;
+			} else {
+				uShift[i] = temp2;
+				borrow = 0;
+			}
+		}
+		temp2 = uShift[i] - carry + borrow;
+		if (temp2 < 0) {
+			uShift[i] = temp2 + BigInteger::kBase;
+			borrow = -1;
+		} else {
+			uShift[i] = temp2;
+			borrow = 0;
+		}
+		if (borrow == 0) res.data[vJ] = qGuess;
+		else { //qGuess на 1 больше нужного
+			res.data[vJ] = qGuess - 1;
+			carry = 0;
+			for (i = 0; i < n; ++i) {
+				if (temp >= BigInteger::kBase) {
+					uShift[i] = temp - BigInteger::kBase;
+					carry = 1;
+				} else {
+					uShift[i] = temp;
+					carry = 0;
+				}
+			}
+			uShift[i] = uShift[i] + carry - BigInteger::kBase;
+		}
+		u.make_right();
+	}
+	res.make_right();
+	if (scale > 1) {
+		std::pair<BigInteger, ui> p = sDiv(b, scale);
+		b = p.first;
+		p = sDiv(u, scale);
+		cur = p.first;
+	} else cur = u;
 }
 
 BigInteger &BigInteger::operator/=(BigInteger const &rhs) {
-BigInteger res;
-BigInteger a = *this, b = rhs;
-if (a < b) {
-return *this = 0;
-}
-res.sign = a.sign ^ b.sign;
-if (b.data.size() == 1) {
-res = myDiv(a, b.data[0]).first;
-} else {
-size_t m = b.data.size();
-while (b.data[m - 1] == 0) {
---m;
-}
-ui J = b.data[m - 1];
-auto f = (ui)(BigInteger::kBase / (J + 1));
-a *= f, b *= f;
-a.make_right();
-b.make_right();
-size_t n = a.data.size();
-m = b.data.size();
-
-while (b.data[m - 1] == 0) {
---m;
-}
-ui divisor = b.data[m - 1];
-
-BigInteger cur = a >> kLogBase * (n - m + 1), aux;
-res.data.resize(n - m + 1);
-
-for (size_t i = n - m + 1; i > 0; --i) {
-cur = (cur << kLogBase) + a.data[i - 1];
-ui tq = getTrial(getNumber(cur.data, m), getNumber(cur.data, m - 1), divisor);
-aux = b * tq;
-while (tq > 0 && cur < aux) {
-aux = b * (--tq);
-}
-cur -= aux;
-res.data[i - 1] = tq;
-}
-}
-
-res.make_right();
-if (a.sign ^ b.sign) {
-res = -res;
-}
-return *this = res;
+	BigInteger b = rhs, res, cur;
+	myDiv(*this, b, res, cur);
+	return res;
 }
 
 BigInteger &BigInteger::operator%=(BigInteger const &rhs) {
-return *this -= *this / rhs * rhs;
+	BigInteger b = rhs, res, cur;
+	myDiv(*this, b, res, cur);
+	return cur;
 }
-
+/*
 BigInteger &BigInteger::apply_bit_operation(BigInteger const &rhs, const std::function<ui(ui, ui)> func) {
-size_t len = std::max(data.size(), rhs.data.size());
-ui emptyCell = getEmptyCell();
-size_t oldSize = data.size();
-data.resize(len);
-while (oldSize < len) {
-data[oldSize++] = emptyCell;
-}
-for (size_t i = 0; i < len; ++i) {
-data[i] = func(i < data.size() ? data[i] : this->getEmptyCell(),
-i < rhs.data.size() ? rhs.data[i] : rhs.getEmptyCell());
-}
-relax();
-return *this;
+	size_t len = std::max(data.size(), rhs.data.size());
+	ui emptyCell = getEmptyCell();
+	size_t oldSize = data.size();
+	data.resize(len);
+	while (oldSize < len) {
+		data[oldSize++] = emptyCell;
+	}
+	for (size_t i = 0; i < len; ++i) {
+		data[i] = func(i < data.size() ? data[i] : this->getEmptyCell(),
+		i < rhs.data.size() ? rhs.data[i] : rhs.getEmptyCell());
+	}
+	relax();
+	return *this;
 }
 
 BigInteger &BigInteger::operator&=(BigInteger const &rhs) {
-return apply_bit_operation(rhs, [](ui a, ui b) { return a & b; });
+	return apply_bit_operation(rhs, [](ui a, ui b) { return a & b; });
 }
 
 BigInteger &BigInteger::operator|=(BigInteger const &rhs) {
-return apply_bit_operation(rhs, [](ui a, ui b) { return a | b; });
+	return apply_bit_operation(rhs, [](ui a, ui b) { return a | b; });
 }
 
 BigInteger &BigInteger::operator^=(BigInteger const &rhs) {
-return apply_bit_operation(rhs, [](ui a, ui b) { return a ^ b; });
+	return apply_bit_operation(rhs, [](ui a, ui b) { return a ^ b; });
 }
 
 void BigInteger::shiftCells(int rhs) {
-if (rhs > 0) {
-size_t oldSize = data.size();
-data.resize(data.size() + rhs);
-for (size_t i = oldSize; i > 0; --i) {
-data[i + rhs - 1] = data[i - 1];
-}
-for (auto i = (size_t)rhs; i > 0; --i) {
-data[i - 1] = 0;
-}
-} else {
-for (auto i = (size_t)-rhs; i < data.size(); ++i) {
-data[i + rhs] = data[i];
-}
-for (size_t i = data.size(); i > data.size() + rhs; --i) {
-data[i - 1] = getEmptyCell();
-}
-relax();
-}
+	if (rhs > 0) {
+		size_t oldSize = data.size();
+		data.resize(data.size() + rhs);
+		for (size_t i = oldSize; i > 0; --i) {
+			data[i + rhs - 1] = data[i - 1];
+		}
+		for (auto i = (size_t)rhs; i > 0; --i) {
+			data[i - 1] = 0;
+		}
+	} else {
+		for (auto i = (size_t)-rhs; i < data.size(); ++i) {
+			data[i + rhs] = data[i];
+		}
+		for (size_t i = data.size(); i > data.size() + rhs; --i) {
+			data[i - 1] = getEmptyCell();
+		}
+		relax();
+	}
 }
 
 BigInteger &BigInteger::operator<<=(int rhs) {
-if (rhs < 0) {
-return *this >>= -rhs;
-}
-int auxShift = rhs / kLogBase;
-if (auxShift) {
-shiftCells(auxShift);
-}
-auxShift = rhs - auxShift * kLogBase;
-if (auxShift) {
-data.push_back(getEmptyCell());
-for (size_t i = data.size(); i > 0; --i) {
-if (i != data.size()) {
-data[i] += data[i - 1] >> (kLogBase - auxShift);
-}
-data[i - 1] <<= auxShift;
-}
-}
-relax();
-return *this;
+	if (rhs < 0) {
+		return *this >>= -rhs;
+	}
+	int auxShift = rhs / kLogBase;
+	if (auxShift) {
+		shiftCells(auxShift);
+	}
+	auxShift = rhs - auxShift * kLogBase;
+	if (auxShift) {
+		data.push_back(getEmptyCell());
+		for (size_t i = data.size(); i > 0; --i) {
+			if (i != data.size()) {
+				data[i] += data[i - 1] >> (kLogBase - auxShift);
+			}
+			data[i - 1] <<= auxShift;
+		}
+	}
+	relax();
+	return *this;
 }
 
 BigInteger &BigInteger::operator>>=(int rhs) {
-if (rhs < 0) {
-return *this <<= -rhs;
+	if (rhs < 0) {
+		return *this <<= -rhs;
+	}
+	int auxShift = rhs / kLogBase;
+	if (auxShift) {
+		shiftCells(-auxShift);
+	}
+	auxShift = rhs - auxShift * kLogBase;
+	if (auxShift) {
+		ui cur = getEmptyCell();
+		for (size_t i = 0; i < data.size(); ++i) {
+			if (i != 0) {
+				data[i - 1] += data[i] << (kLogBase - auxShift);
+			}
+			data[i] >>= auxShift;
+		}
+		data.back() += cur << (kLogBase - auxShift);
+	}
+	relax();
+	return *this;
 }
-int auxShift = rhs / kLogBase;
-if (auxShift) {
-shiftCells(-auxShift);
-}
-auxShift = rhs - auxShift * kLogBase;
-if (auxShift) {
-ui cur = getEmptyCell();
-for (size_t i = 0; i < data.size(); ++i) {
-if (i != 0) {
-data[i - 1] += data[i] << (kLogBase - auxShift);
-}
-data[i] >>= auxShift;
-}
-data.back() += cur << (kLogBase - auxShift);
-}
-relax();
-return *this;
-}
+*/
 
 BigInteger BigInteger::operator+() const {
-return *this;
+	return *this;
 }
 
 BigInteger BigInteger::operator-() const {
-return ~*this + 1;
+	return ~*this + 1;
 }
 
 BigInteger BigInteger::operator~() const {
-BigInteger r = *this;
-if (data.empty()) {
-r.data.push_back(0);
-}
-for (size_t i = 0; i < r.data.size(); ++i) {
-r.data[i] = ~r.data[i];
-}
-r.relax();
-return r;
+	BigInteger r = *this;
+	if (data.empty()) {
+		r.data.push_back(0);
+	}
+	for (size_t i = 0; i < r.data.size(); ++i) {
+		r.data[i] = ~r.data[i];
+	}
+	r.make_right();
+	return r;
 }
 
 BigInteger &BigInteger::operator++() {
-return *this += 1;
+	return *this += 1;
 }
 
 const BigInteger BigInteger::operator++(int) {
-BigInteger r = *this;
-++*this;
-return r;
+	BigInteger r = *this;
+	++*this;
+	return r;
 }
 
 BigInteger &BigInteger::operator--() {
-return *this -= 1;
+	return *this -= 1;
 }
 
 const BigInteger BigInteger::operator--(int) {
-BigInteger r = *this;
---*this;
-return r;
+	BigInteger r = *this;
+	--*this;
+	return r;
 }
 
 BigInteger operator+(BigInteger a, BigInteger const &b) {
-return a += b;
+	return a += b;
 }
 
 BigInteger operator-(BigInteger a, BigInteger const &b) {
-return a -= b;
+	return a -= b;
 }
 
 BigInteger operator*(BigInteger a, BigInteger const &b) {
-return a *= b;
+	return a *= b;
 }
 
 BigInteger operator/(BigInteger a, BigInteger const &b) {
-return a /= b;
+	return a /= b;
 }
 
 BigInteger operator%(BigInteger a, BigInteger const &b) {
-return a %= b;
+	return a %= b;
 }
 
+/*
 BigInteger operator&(BigInteger a, BigInteger const &b) {
-return a &= b;
+	return a &= b;
 }
 
 BigInteger operator|(BigInteger a, BigInteger const &b) {
-return a |= b;
+	return a |= b;
 }
 
 BigInteger operator^(BigInteger a, BigInteger const &b) {
-return a ^= b;
+	return a ^= b;
 }
 
 BigInteger operator<<(BigInteger a, int b) {
-return a <<= b;
+	return a <<= b;
 }
 
 BigInteger operator>>(BigInteger a, int b) {
-return a >>= b;
+	return a >>= b;
 }
 
 bool operator==(BigInteger const &a, BigInteger const &b) {
-return a.data == b.data;
+	return a.data == b.data;
 }
 
 bool operator!=(BigInteger const &a, BigInteger const &b) {
-return !(a.data == b.data);
+	return !(a.data == b.data);
 }
-
+*/
+/*
 bool operator<(BigInteger const &a, BigInteger const &b) {
-if (a.getSign() != b.getSign()) {
-return a.getSign();
-}
-if (a.data.size() != b.data.size()) {
-return a.data.size() < b.data.size();
-}
-for (size_t i = a.data.size(); i > 0; --i) {
-if (a.data[i - 1] != b.data[i - 1]) {
-return a.data[i - 1] < b.data[i - 1];
-}
-}
-return false;
+	if (a.sign != b.sign) {
+		return a.sign;
+	}
+	if (a.data.size() != b.data.size()) {
+		return a.data.size() < b.data.size();
+	}
+	for (size_t i = a.data.size(); i > 0; --i) {
+		if (a.data[i - 1] != b.data[i - 1]) {
+			return a.data[i - 1] < b.data[i - 1];
+		}
+	}
+	return false;
 }
 
 bool operator>(BigInteger const &a, BigInteger const &b) {
-return b < a;
+	return b < a;
 }
-
 bool operator<=(BigInteger const &a, BigInteger const &b) {
-return !(b < a);
+	return !(b < a);
 }
-
 bool operator>=(BigInteger const &a, BigInteger const &b) {
-return !(a < b);
+	return !(a < b);
 }
-
-*/
 
 std::string to_string(BigInteger const &a) {
 	return a.to_string();
 }
 
 
-	
+
 std::ostream &operator<<(std::ostream &s, BigInteger const &a) {
 	return s << a.to_string();
 }
@@ -569,3 +591,4 @@ std::istream &operator>>(std::istream &s, BigInteger &a) {
 	a = BigInteger(aux);
 	return s;
 }
+*/
